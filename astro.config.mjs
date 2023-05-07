aba3096f963be488cff31c21cb741e9d24bb275c
Rafael Pinto <archlinuxrp@gmail.com>
2023-05-07 13:00:57 +0100

    inicial docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import preact from '@astrojs/preact';
+import react from '@astrojs/react';
 
 export default defineConfig({
   site: "https://astroship.web3templates.com",
@@ -13,5 +15,9 @@ export default defineConfig({
     }),
     mdx(),
     sitemap(),
+    // Enable Preact to support Preact JSX components.
+		preact(),
+		// Enable React for the Algolia search component.
+		react(),
   ],
 });
