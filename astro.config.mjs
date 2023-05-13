fed8419fa7f0c18096967b7444c44b5dda3e87e4
Rafael Pinto <archlinuxrp@gmail.com>
2023-05-13 11:16:11 +0100

    netlyfy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,19 +5,18 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import preact from '@astrojs/preact';
 import react from '@astrojs/react';
+import netlify from "@astrojs/netlify/functions";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-    sitemap(),
-    // Enable Preact to support Preact JSX components.
-		preact(),
-		// Enable React for the Algolia search component.
-		react(),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), mdx(), sitemap(),
+  // Enable Preact to support Preact JSX components.
+  preact(),
+  // Enable React for the Algolia search component.
+  react()],
+  output: "server",
+  adapter: netlify()
 });
