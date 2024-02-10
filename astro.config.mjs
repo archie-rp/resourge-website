177552e4012832be61bc826a9b347da710112ec1
rafaelpinto-devoteam <79202214+rafaelpinto-devoteam@users.noreply.github.com>
2024-02-10 12:32:54 +0000

    fix deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
@@ -20,7 +20,6 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
   ],
-  output: 'server',
   adapter: vercel({
     webAnalytics: {
       enabled: true
