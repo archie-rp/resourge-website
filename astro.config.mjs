cc87f33cd4b3f4e968f0b79920aaae6f9fe0398a
rafaelpinto-devoteam <79202214+rafaelpinto-devoteam@users.noreply.github.com>
2024-02-10 11:18:24 +0000

    fix sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
 import vercel from '@astrojs/vercel/static';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-nomy.vercel.app",
+  site: "https://resourge.vercel.app",
   integrations: [
     mdx({
       syntaxHighlight: 'shiki',
